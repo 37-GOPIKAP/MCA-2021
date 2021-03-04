51255f4c75b5e24cf6d58f19870ef1418708cc84
gopugrasp <62229869+gopugrasp@users.noreply.github.com>
2021-03-04 16:45:44 +0530

    Update 4_count_the_occurance_of_each_word.py

--- PYTHON/CO1/4_count_the_occurance_of_each_word.py
+++ PYTHON/CO1/4_count_the_occurance_of_each_word.py
@@ -1,3 +1,4 @@
+#count the occurance of each word in a line of text
 text="Python is a popular programming language which is developed by Guido Van Rossum "
-for i in text.strip().split():
-    print("Number of occurence of word ","\"",i,"\""," is :",text.strip().split().count(i))
+for i in text.split():
+    print("Number of occurence of word ","\"",i,"\""," is :",text.split().count(i))
