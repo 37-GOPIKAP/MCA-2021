fd216222e3dba0d29953ad3bbab62bd4c02f730c
GOPIKA8299 <62229869+GOPIKA8299@users.noreply.github.com>
2021-03-24 15:12:17 +0530

    new php program added

---
+++ WEB PROGRAMING LAB/noof.php
@@ -0,0 +1,14 @@
+
+<?php
+session_start();
+if(isset($_SESSION['counter']))
+{
+$_SESSION['counter']+=1;
+}
+else
+{
+$_SESSION['counter']=1;
+}
+echo"you have visited this page",$_SESSION['counter'],"time in this session";
+?>
+
